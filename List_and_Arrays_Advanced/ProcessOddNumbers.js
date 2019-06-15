bc4c34b7ca8efa33a5c386af805e2d8cc3ab179e
ivo tsochev <ivo.tsochev@live.com>
2019-06-16 00:53:06 +0300

    advanced arrays

---
+++ List_and_Arrays_Advanced/ProcessOddNumbers.js
@@ -0,0 +1,24 @@
+function oddNumbers(arr) {
+    let orderedArr = arr.filter((element, index) => {
+        return (index % 2 !== 0);
+    });
+
+    // console.log(orderedArr);
+    
+    // orderedArr = orderedArr.toString();
+    
+    let doubleNums = [];
+    for (const element of orderedArr) {
+        doubleNums.push(Number(element) * 2);
+    }
+    
+    // console.log(doubleNums);
+
+    doubleNums = doubleNums.reverse().toString().replace(/,/g, " ");
+    console.log(doubleNums).sort;
+
+
+    
+}
+
+oddNumbers([3, 0, 10, 4, 7, 3]);
---
+++ List_and_Arrays_Advanced/SmallestTwoNumbers.js
@@ -0,0 +1,8 @@
+function smallestTwoNums(arr) {
+    let workingArr = arr.slice();
+    console.log(workingArr);
+    
+    
+}
+
+smallestTwoNums([30, 15, 50, 5]);
