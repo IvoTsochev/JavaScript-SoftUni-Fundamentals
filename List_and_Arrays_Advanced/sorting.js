330d62ee581124272f3a98870bdaa0e103195153
Ivo Tsochev <ivo.tsochev@live.com>
2019-06-18 20:38:22 +0300

    advanced array

--- List_and_Arrays_Advanced/sorting.js
+++ List_and_Arrays_Advanced/sorting.js
@@ -1,2 +1,24 @@
 //първо сортираме по низходящо и полсе shift и pop
+function solve(input) {
+    let inputCopy = input.slice().sort((a, b) => b - a);
+    console.log(inputCopy);
+    let newArr = [];
+    
 
+    for (let i = 0; i < inputCopy.length; i++) {
+        newArr += inputCopy.shift(inputCopy[i]) + " ";
+        newArr += inputCopy.pop(inputCopy[i]) + " ";
+
+        
+    }
+    
+
+    newArr += inputCopy.shift() + " ";
+    newArr += inputCopy.shift();
+    
+    
+    console.log(newArr);
+    
+}
+
+solve([1, 21, 3, 52, 69, 63, 31, 2, 18, 94]);
