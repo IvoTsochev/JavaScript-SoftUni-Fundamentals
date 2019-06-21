d54e500b085f881f6828b028738c32687ebaf6db
ivo tsochev <ivo.tsochev@live.com>
2019-06-21 15:07:41 +0300

    another update

---
+++ List_and_Arrays_Advanced/searcForaNumber.js
@@ -0,0 +1,13 @@
+function solve(arr1, params) {
+    let elementsToTake = params[0];
+    let elementToRemove = params[1];
+    console.log(elementToRemove);
+    
+    let newArr = arr1.slice(0, elementsToTake);
+
+    
+}
+
+solve(  [5, 2, 3, 4, 1, 6],
+        [5, 2, 3]
+     );
