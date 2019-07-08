f77bc810c1d02b6b30e94ffacbcad436272cfdd4
Ivo Tsochev <ivo.tsochev@live.com>
2019-07-08 20:38:02 +0300

    ass arrays

---
+++ Associative_Arrays/cardGame.js
@@ -0,0 +1,25 @@
+function solve (input) {
+
+}
+
+solve([ 'this sentence',
+'In',
+'this',
+'sentence',
+'you',
+'have',
+'to',
+'count',
+'the',
+'occurances',
+'of',
+'the',
+'words',
+'this',
+'and',
+'sentence',
+'because',
+'this',
+'is',
+'your',
+'task' ];)
